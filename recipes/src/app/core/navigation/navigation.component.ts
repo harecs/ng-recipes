85c2ff49fa53fb1c12e13602ef8abfcbce27a8c3
Zahari Lazarov <harilazaro@gmail.com>
2024-03-14 11:29:44 +0200

    Add navigation component

--- recipes/src/app/core/navigation/navigation.component.ts
+++ recipes/src/app/core/navigation/navigation.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./navigation.component.css']
 })
 export class NavigationComponent {
+  isSidebarShowed: boolean = false;
 
+  w3_open(): void {
+    this.isSidebarShowed = !this.isSidebarShowed;
+  }
+
+  w3_close(): void {
+    this.isSidebarShowed = false;
+  }
 }

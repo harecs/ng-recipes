89fb048f73b0941bad1e2638f8a2eca430817298
Zahari Lazarov <harilazaro@gmail.com>
2024-03-14 16:33:48 +0200

    Add more environment variables

--- recipes/src/environments/environment.development.ts
+++ recipes/src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
     APPLICATION_ID: 'UsFhrX1gcOiyNSQC2HdkGv9tM515PKek7J60hixb',
-    REST_API_KEY: 'RgVk31EXwCbxNyqBGmlXrjNGNlDFJAbXE21Y3lDC'
+    REST_API_KEY: 'RgVk31EXwCbxNyqBGmlXrjNGNlDFJAbXE21Y3lDC',
+    RECIPE_CLASS_URL: 'https://parseapi.back4app.com/classes/Recipe'
 };

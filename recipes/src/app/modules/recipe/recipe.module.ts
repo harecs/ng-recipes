a68c6beacb02763667d3b56052e656eb2d42ae10
Zahari Lazarov <harilazaro@gmail.com>
2024-03-15 10:42:55 +0200

    Generate recipe details component

--- recipes/src/app/modules/recipe/recipe.module.ts
+++ recipes/src/app/modules/recipe/recipe.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RecipesComponent } from './components/recipes/recipes.component';
+import { RecipeService } from '../recipe.service';
+import { RecipeDetailsComponent } from './components/recipe-details/recipe-details.component';
 
 
 
 @NgModule({
   declarations: [
-    RecipesComponent
+    RecipesComponent,
+    RecipeDetailsComponent
   ],
   imports: [
     CommonModule
